790fb60ddd8be9b06b3e3c23a35c8fab67508fb8
HexagonSun <HexagonSun@users.noreply.github.com>
2016-12-02 07:14:05 +0100

    Day 02 - part 02

--- src/main/java/net/hexagon/sun/aoc/Day02.java
+++ src/main/java/net/hexagon/sun/aoc/Day02.java
@@ -8,13 +8,19 @@ import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-// http://adventofcode.com/2016/day/1
+// http://adventofcode.com/2016/day/2
 public class Day02 extends AdventOfCode {
 
 	enum Pad {
 		ONE, TWO, THREE,
 		FOUR, FIFE, SIX,
-		SEVEN, EIGHT, NINE;
+		SEVEN, EIGHT, NINE,
+
+						ONE_2,
+				TWO_2, THREE_2, FOUR_2,
+		FIFE_2, SIX_2, SEVEN_2, EIGHT_2, NINE_2,
+				A_2, 	B_2, 	C_2,
+						D_2;
 
 		Pad up;
 		Pad right;
@@ -32,6 +38,21 @@ public class Day02 extends AdventOfCode {
 			SEVEN.directions(FOUR, EIGHT, SEVEN, SEVEN);
 			EIGHT.directions(FIFE, NINE, EIGHT, SEVEN);
 			NINE.directions(SIX, NINE, NINE, EIGHT);
+
+
+			ONE_2.directions(ONE_2, ONE_2, THREE_2, ONE_2);
+			TWO_2.directions(TWO_2, THREE_2, SIX_2, TWO_2);
+			THREE_2.directions(ONE_2, FOUR_2, SEVEN_2, TWO_2);
+			FOUR_2.directions(FOUR_2, FOUR_2, EIGHT_2, THREE_2);
+			FIFE_2.directions(FIFE_2, SIX_2, FIFE_2, FIFE_2);
+			SIX_2.directions(TWO_2, SEVEN_2, A_2, FIFE_2);
+			SEVEN_2.directions(THREE_2, EIGHT_2, B_2, SIX_2);
+			EIGHT_2.directions(FOUR_2, NINE_2, C_2, SEVEN_2);
+			NINE_2.directions(NINE_2, NINE_2, NINE_2, EIGHT_2);
+			A_2.directions(SIX_2, B_2, A_2, A_2);
+			B_2.directions(SEVEN_2, C_2, D_2, A_2);
+			C_2.directions(EIGHT_2, C_2, C_2, B_2);
+			D_2.directions(B_2, D_2, D_2, D_2);
 		}
 
 		private void directions(Pad up, Pad right, Pad down, Pad left) {
@@ -62,7 +83,8 @@ public class Day02 extends AdventOfCode {
 	@Test
 	@Override
 	public void runTask2 () {
-		solveTask2(getInputLines());
+		String solution= "A7AC3";
+		assertThat(solveTask2(getInputLines()), is(solution));
 	}
 
 	@Test
@@ -71,20 +93,30 @@ public class Day02 extends AdventOfCode {
 		assertThat(solveTask1(input), is("1985"));
 	}
 
+	@Test
+	public void runTask2Example1() {
+		List<String> input= Arrays.asList("ULL\n", "RRDDD\n", "LURDL\n", "UUUUD");
+		assertThat(solveTask2(input), is("5DB3"));
+	}
+
 	private String solveTask1 (List<String> input) {
-		Pad button= Pad.FIFE;
+		return solve(input, Pad.FIFE, 1);
+	}
+
+	private String solveTask2 (List<String> input) {
+		return solve(input, Pad.FIFE_2, -8);
+	}
+
+	private String solve(List<String> input, Pad start, int ordinalOffset) {
+		Pad button= start;
 		StringBuilder solution= new StringBuilder();
 		for (String line : input) {
 			for (char step : line.trim().toCharArray()) {
 				button= button.getNext(step);
 			}
-			solution.append(button.ordinal() + 1);
+			solution.append(Integer.toHexString(button.ordinal() + ordinalOffset).toUpperCase());
 		}
 		return solution.toString();
-	}
-
-	private String solveTask2 (List<String> input) {
-		return "";
 	}
 
 }
