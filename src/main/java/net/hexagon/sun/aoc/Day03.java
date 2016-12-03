a19d393d5b1138cb9a61e0bc32117ccbaa8facc4
HexagonSun <HexagonSun@users.noreply.github.com>
2016-12-03 09:30:35 +0100

    Day 03 - part 02

--- src/main/java/net/hexagon/sun/aoc/Day03.java
+++ src/main/java/net/hexagon/sun/aoc/Day03.java
@@ -2,7 +2,9 @@ package net.hexagon.sun.aoc;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -25,7 +27,7 @@ public class Day03 extends AdventOfCode {
 	@Test
 	@Override
 	public void runTask2 () {
-		int solution= -1;
+		int solution= 1921;
 		assertThat(solveTask2(getInputLines()), is(solution));
 	}
 
@@ -43,8 +45,14 @@ public class Day03 extends AdventOfCode {
 
 	@Test
 	public void runTask2Example1() {
-		List<String> input= Collections.singletonList("  5  10  25");
-		assertThat(solveTask2(input), is(0));
+		List<String> input= Arrays.asList(
+				"101 301 501",
+				"102 302 502",
+				"103 303 503",
+				"201 401 601",
+				"202 402 602",
+				"203 403 603");
+		assertThat(solveTask2(input), is(6));
 	}
 
 	private int solveTask1 (List<String> input) {
@@ -56,12 +64,16 @@ public class Day03 extends AdventOfCode {
 	private boolean isTriangle(String line) {
 		Matcher m = TRIANGLE_PATTERN.matcher(line);
 		if (m.find()) {
-			return isTriangle(
-					Integer.valueOf(m.group(1)),
-					Integer.valueOf(m.group(2)),
-					Integer.valueOf(m.group(3)));
+			return isTriangle(m.group(1), m.group(2), m.group(3));
 		}
 		return false;
+	}
+
+	private boolean isTriangle(String a, String b, String c) {
+		return isTriangle(
+				Integer.valueOf(a),
+				Integer.valueOf(b),
+				Integer.valueOf(c));
 	}
 
 	private boolean isTriangle(int a, int b, int c) {
@@ -72,7 +84,27 @@ public class Day03 extends AdventOfCode {
 	}
 
 	private int solveTask2 (List<String> input) {
-		return -1;
+		int count= 0;
+		// we checked the input length and know that we can read in groups of three lines
+		for (Iterator<String> it= input.iterator(); it.hasNext();) {
+			String line1 = it.next();
+			String line2 = it.next();
+			String line3 = it.next();
+
+			Matcher m1= TRIANGLE_PATTERN.matcher(line1);
+			Matcher m2= TRIANGLE_PATTERN.matcher(line2);
+			Matcher m3= TRIANGLE_PATTERN.matcher(line3);
+
+			if (m1.find() && m2.find() && m3.find()) {
+				count += isTriangle(m1.group(1), m2.group(1), m3.group(1)) ? 1 : 0;
+				count += isTriangle(m1.group(2), m2.group(2), m3.group(2)) ? 1 : 0;
+				count += isTriangle(m1.group(3), m2.group(3), m3.group(3)) ? 1 : 0;
+			} else {
+				System.out.println("failed to parse input data");
+				return -1;
+			}
+		}
+		return count;
 	}
 
 }
