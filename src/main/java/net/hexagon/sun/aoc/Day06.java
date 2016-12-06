03e8dce848f8fbe342a451156e8091230e65f6bc
HexagonSun <HexagonSun@users.noreply.github.com>
2016-12-06 06:22:18 +0100

    Day 06 - part 02

--- src/main/java/net/hexagon/sun/aoc/Day06.java
+++ src/main/java/net/hexagon/sun/aoc/Day06.java
@@ -29,6 +29,19 @@ public class Day06 extends AdventOfCode {
 			}
 			return (char) ('a' + maxIdx);
 		}
+
+		char getLeastFrequent() {
+			int minCount= Integer.MAX_VALUE;
+			int minIdx= 0;
+			for (int i = 0; i < 26; i++) {
+				int value= counter[i];
+				if (value > 0 && value < minCount) {
+					minCount= value;
+					minIdx= i;
+				}
+			}
+			return (char) ('a' + minIdx);
+		}
 	}
 
 	@Test
@@ -41,35 +54,61 @@ public class Day06 extends AdventOfCode {
 	@Test
 	@Override
 	public void runTask2 () {
+		String solution= "akothqli";
+		assertThat(solveTask2(getInputLines()), is(solution));
 	}
 
 	@Test
 	public void runExample1() {
 		List<String> input= Arrays.asList("eedadn",
-							  "drvtee",
-							  "eandsr",
-							  "raavrd",
-							  "atevrs",
-							  "tsrnev",
-							  "sdttsa",
-							  "rasrtv",
-							  "nssdts",
-							  "ntnada",
-							  "svetve",
-							  "tesnvt",
-							  "vntsnd",
-							  "vrdear",
-							  "dvrsen",
-							  "enarar");
+				"drvtee",
+				"eandsr",
+				"raavrd",
+				"atevrs",
+				"tsrnev",
+				"sdttsa",
+				"rasrtv",
+				"nssdts",
+				"ntnada",
+				"svetve",
+				"tesnvt",
+				"vntsnd",
+				"vrdear",
+				"dvrsen",
+				"enarar");
 		assertThat(solveTask1(input), is("easter"));
 	}
 
 	@Test
 	public void runTask2Example1() {
+		List<String> input= Arrays.asList("eedadn",
+				"drvtee",
+				"eandsr",
+				"raavrd",
+				"atevrs",
+				"tsrnev",
+				"sdttsa",
+				"rasrtv",
+				"nssdts",
+				"ntnada",
+				"svetve",
+				"tesnvt",
+				"vntsnd",
+				"vrdear",
+				"dvrsen",
+				"enarar");
+		assertThat(solveTask2(input), is("advent"));
 	}
 
 	private String solveTask1 (List<String> input) {
+		return solve(input, true);
+	}
 
+	private String solveTask2 (List<String> input) {
+		return solve(input, false);
+	}
+
+	private String solve (List<String> input, boolean useMostLikely) {
 		int wordLength= input.get(0).length();
 		Frequency[] frequencies= new Frequency[wordLength];
 		for (int i = 0; i < wordLength; i++) {
@@ -86,12 +125,12 @@ public class Day06 extends AdventOfCode {
 
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < wordLength; i++) {
-			sb.append(frequencies[i].getMostFrequent());
+			if (useMostLikely) {
+				sb.append(frequencies[i].getMostFrequent());
+			} else {
+				sb.append(frequencies[i].getLeastFrequent());
+			}
 		}
 		return sb.toString();
-	}
-
-	private String solveTask2 (List<String> input) {
-		return "";
 	}
 }
