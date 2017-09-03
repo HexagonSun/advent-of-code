b96ce4e0981cb9d40aea8d0c376f3b1109dd7896
HexagonSun <HexagonSun@users.noreply.github.com>
2017-09-03 14:53:18 +0200

    Day 24 - initial outline

---
+++ src/main/java/net/hexagon/sun/aoc/v2015/Day24.java
@@ -0,0 +1,17 @@
+package net.hexagon.sun.aoc.v2015;
+
+import net.hexagon.sun.aoc.AdventOfCode;
+
+public class Day24 extends AdventOfCode {
+
+	@Override
+	public void runTask1() {
+
+	}
+
+	@Override
+	public void runTask2() {
+
+	}
+
+}
