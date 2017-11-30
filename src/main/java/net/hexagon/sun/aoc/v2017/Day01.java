0d42949e4fdb33c6e88d407066aa1caeb45aee65
HexagonSun <HexagonSun@users.noreply.github.com>
2017-11-30 19:14:12 +0100

    Day 01 - initial skeleton

---
+++ src/main/java/net/hexagon/sun/aoc/v2017/Day01.java
@@ -0,0 +1,17 @@
+package net.hexagon.sun.aoc.v2017;
+
+import net.hexagon.sun.aoc.AdventOfCode;
+
+public class Day01 extends AdventOfCode {
+
+	@Override
+	public void runTask1() {
+
+	}
+
+	@Override
+	public void runTask2() {
+
+	}
+
+}
