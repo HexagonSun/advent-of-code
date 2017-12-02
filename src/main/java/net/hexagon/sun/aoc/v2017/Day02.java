0726d6937612a0548c7c064ebde22538d1487ca6
HexagonSun <HexagonSun@users.noreply.github.com>
2017-12-02 08:26:36 +0100

    Day 02 - part 2

--- src/main/java/net/hexagon/sun/aoc/v2017/Day02.java
+++ src/main/java/net/hexagon/sun/aoc/v2017/Day02.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -14,12 +15,13 @@ public class Day02 extends AdventOfCode {
 	@Test
 	@Override
 	public void runTask1() {
-		assertThat(solveTask1(getInputLines()), is(18));
+		assertThat(solveTask1(getInputLines()), is(51833));
 	}
 
 	@Test
 	@Override
 	public void runTask2() {
+		assertThat(solveTask2(getInputLines()), is(288));
 	}
 
 	@Test
@@ -31,15 +33,32 @@ public class Day02 extends AdventOfCode {
 		assertThat(solveTask1(lines), is(18));
 	}
 
+	@Test
+	public void runTask2Example1() {
+		String input = "5	9	2	8\n" +
+					   "9	4	7	3\n" +
+					   "3	8	6	5";
+		List<String> lines= Arrays.asList(input.split("\n"));
+		assertThat(solveTask2(lines), is(9));
+	}
+
 	private int solveTask1(List<String> lines) {
+		return solve(lines, this::maxDiff);
+	}
+
+	private int solveTask2(List<String> lines) {
+		return solve(lines, this::evenlyDivisible);
+	}
+
+	private int solve(List<String> lines, Function<String[], Integer> algorithm) {
 		return lines.stream()
-					   .map(this::maxDiff)
+					   .map(s -> s.split("\t"))
+					   .map(algorithm)
 					   .mapToInt(i -> i)
 					   .sum();
 	}
 
-	private Integer maxDiff(String line) {
-		String[] raw= line.split("\t");
+	private Integer maxDiff(String[] raw) {
 		int min= Integer.MAX_VALUE;
 		int max= Integer.MIN_VALUE;
 		for (String s : raw) {
@@ -48,6 +67,24 @@ public class Day02 extends AdventOfCode {
 			max= i > max ? i : max;
 		}
 		return max - min;
+	}
+
+	private Integer evenlyDivisible(String[] raw) {
+		int sum= 0;
+		for (int i = 0; i < raw.length; i++) {
+			Integer a= Integer.valueOf(raw[i]);
+
+			for (int j = 0; j < raw.length; j++) {
+				if (i == j) { continue; }
+				Integer b= Integer.valueOf(raw[j]);
+				if (a < b) { continue; }
+
+				if ((a % b) == 0) {
+					sum+= a / b;
+				}
+			}
+		}
+		return sum;
 	}
 
 }
