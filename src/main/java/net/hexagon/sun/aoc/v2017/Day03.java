d1d84d3c73223174bf0e8691ce24e6384f60faaa
HexagonSun <HexagonSun@users.noreply.github.com>
2017-12-03 11:01:42 +0100

    Day 03 - part 2

--- src/main/java/net/hexagon/sun/aoc/v2017/Day03.java
+++ src/main/java/net/hexagon/sun/aoc/v2017/Day03.java
@@ -19,7 +19,7 @@ public class Day03 extends AdventOfCode {
 	@Test
 	@Override
 	public void runTask2() {
-
+		assertThat(solveTask2(new int[64][64], 325489), is(330785));
 	}
 
 	@Test
@@ -32,8 +32,21 @@ public class Day03 extends AdventOfCode {
 
 	@Test
 	public void runTask2Example1() {
-
-		assertThat(solveTask2(new int[100][100], 1), is(2));
+		//		147  142  133  122   59
+		//		304    5    4    2   57
+		//		330   10    1    1   54
+		//		351   11   23   25   26
+		//		362  747  806--->   ...
+		assertThat(solveTask2(new int[8][8], 1), is(2));
+		assertThat(solveTask2(new int[8][8], 2), is(4));
+		assertThat(solveTask2(new int[8][8], 4), is(5));
+		assertThat(solveTask2(new int[8][8], 10), is(11));
+		assertThat(solveTask2(new int[8][8], 11), is(23));
+		assertThat(solveTask2(new int[8][8], 26), is(54));
+		assertThat(solveTask2(new int[8][8], 59), is(122));
+		assertThat(solveTask2(new int[8][8], 147), is(304));
+		assertThat(solveTask2(new int[8][8], 362), is(747));
+		assertThat(solveTask2(new int[8][8], 747), is(806));
 	}
 
 	private int solveTask1(int target) {
@@ -41,7 +54,39 @@ public class Day03 extends AdventOfCode {
 	}
 
 	private int solveTask2(int[][] grid, int threshold) {
+		Point origin= new Point(grid.length / 2, grid.length / 2);
+		store(grid, origin, 1);
+		int maxSteps= 1000;
+		for (int i = 2; i < maxSteps; i++) {
+			Point spiralPoint= spiral(i);
+			Point target= withOrigin(origin, spiralPoint);
+			int value= sumOfNeighbours(grid, target);
+			if (value > threshold) {
+//				System.out.println("Found first value larger than " + threshold + " to be " + value);
+//				System.out.println("\tCoordinates: " + target);
+//				System.out.println("\tSpiral point: " + spiralPoint);
+				return value;
+			}
+			store(grid, target, value);
+		}
 		return -1;
+	}
+
+	private void store(int[][] grid, Point target, int value) {
+		grid[target.x][target.y]= value;
+	}
+
+	private int sumOfNeighbours(int[][] grid, Point target) {
+		int sum= 0;
+		sum+= grid[target.x + 1][target.y];
+		sum+= grid[target.x + 1][target.y + 1];
+		sum+= grid[target.x][target.y + 1];
+		sum+= grid[target.x - 1][target.y + 1];
+		sum+= grid[target.x - 1][target.y];
+		sum+= grid[target.x - 1][target.y - 1];
+		sum+= grid[target.x][target.y - 1];
+		sum+= grid[target.x + 1][target.y - 1];
+		return sum;
 	}
 
 	// https://math.stackexchange.com/a/163101
@@ -77,6 +122,10 @@ public class Day03 extends AdventOfCode {
 	// taxicab distance
 	private int distance(Point p) {
 		return Math.abs(p.x) + Math.abs(p.y);
+	}
+
+	private Point withOrigin(Point origin, Point target) {
+		return new Point(origin.x + target.x, origin.y + target.y);
 	}
 
 }
