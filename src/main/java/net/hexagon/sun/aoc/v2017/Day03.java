9d0d3c1c3afaed0bdd3fcf0ff6f71d763b14a6ea
HexagonSun <HexagonSun@users.noreply.github.com>
2017-12-03 10:26:59 +0100

    Day 03 - part 2 * Refactoring, extract logic to determine point on grid/spiral

--- src/main/java/net/hexagon/sun/aoc/v2017/Day03.java
+++ src/main/java/net/hexagon/sun/aoc/v2017/Day03.java
@@ -3,6 +3,8 @@ package net.hexagon.sun.aoc.v2017;
 import net.hexagon.sun.aoc.AdventOfCode;
 import org.junit.Test;
 
+import java.awt.*;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -23,62 +25,58 @@ public class Day03 extends AdventOfCode {
 	@Test
 	public void runExample1() {
 		assertThat(solveTask1(1), is(0));
-	}
-
-	@Test
-	public void runExample2() {
 		assertThat(solveTask1(12), is(3));
-	}
-
-	@Test
-	public void runExample3() {
 		assertThat(solveTask1(23), is(2));
-	}
-
-	@Test
-	public void runExample4() {
 		assertThat(solveTask1(1024), is(31));
 	}
 
 	@Test
 	public void runTask2Example1() {
 
+		assertThat(solveTask2(new int[100][100], 1), is(2));
 	}
 
-
 	private int solveTask1(int target) {
-		return spiral(target);
+		return distance(spiral(target));
+	}
+
+	private int solveTask2(int[][] grid, int threshold) {
+		return -1;
 	}
 
 	// https://math.stackexchange.com/a/163101
-	private int spiral(int target) {
+	private Point spiral(int target) {
 		double k= Math.ceil((Math.sqrt(target)-1)/2);
 		double t= 2*k+1;
 		double m= t * t;
 		t=t-1;
 
 		if (target >= m-t) {
-			return distance(k-(m-target), -k);
+			return point(k-(m-target), -k);
 		} else {
 			m=m-t;
 		}
 
 		if (target >= m-t) {
-			return distance(-k, -k+(m-target));
+			return point(-k, -k+(m-target));
 		} else  {
 			m=m-t;
 		}
 
 		if (target >= m-t) {
-			return distance(-k+(m-target), k);
+			return point(-k+(m-target), k);
 		}  else  {
-			return distance(k, k-(m-target-t));
+			return point(k, k-(m-target-t));
 		}
 	}
 
+	private Point point(double x, double y) {
+		return new Point((int)x, (int)y);
+	}
+
 	// taxicab distance
-	private int distance(double x, double y) {
-		return Math.abs((int)x) + Math.abs((int)y);
+	private int distance(Point p) {
+		return Math.abs(p.x) + Math.abs(p.y);
 	}
 
 }
